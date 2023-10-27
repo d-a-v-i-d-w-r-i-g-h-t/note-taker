3ab7405572c1970c13454ac11513ffda1486faca
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-27 14:00:26 -0700

    Add pseudocode

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -85,7 +85,8 @@ const handleNoteSave = () => {
 
 // Delete the clicked note
 const handleNoteDelete = (e) => {
-  // Prevents the click listener for the list from being called when the button inside of it is clicked
+  // Prevents the click listener for the list from being called
+  // when the button inside of it is clicked
   e.stopPropagation();
 
   const note = e.target;
