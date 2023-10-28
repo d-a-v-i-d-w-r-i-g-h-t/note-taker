d0faf7884cd4033b4a2a629bb6e09da48f7f0596
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-27 22:44:06 -0700

    Clean up - add comments, remove pseudocode file

--- helpers/fsUtilities.js
+++ helpers/fsUtilities.js
@@ -4,31 +4,25 @@ const util = require('util');
 // promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
-
-
+// use fs writefile to write stringified content to the argument destination
 const writeToFile = (destination, content) => {
-  console.log("write: " + destination, content);
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 };
 
+// use fs readfile to retrieve the current file, append a new element, and write the new file
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
   
-
-  const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
-  
-  
-    
-
 
 module.exports = { readFromFile, writeToFile, readAndAppend };
